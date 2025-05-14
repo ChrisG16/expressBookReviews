b2ab7eb9050a6e37934ee66ee7b68b097bfc28c5
ChrisG16 <christiang3750@outlook.com>
2025-05-13 21:01:59 -0400

    Book database

---
+++ router/booksdb.js
@@ -0,0 +1,14 @@
+let books = {
+      1: {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": {"Alex Delmont": "11/10, Masterpiece. I recommend it!"} },
+      2: {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": {"Jorge de Guzman": "It takes me 1 month to understand but I recommend it"} },
+      3: {"author": "Dante Alighieri","title": "The Divine Comedy", "reviews": {"Diane Dortmund": "I did not understand much of the book but I recommend it."} },
+      4: {"author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": {"Ronald Gump": "Good book, I'll buy 10 copies."} },
+      5: {"author": "Unknown","title": "The Book Of Job", "reviews": {"Analisa Meltrozo": "Wonderful book."} },
+      6: {"author": "Unknown","title": "One Thousand and One Nights", "reviews": {"Melton Husk": "My head exploded when reading it."} },
+      7: {"author": "Unknown","title": "Nj\u00e1l's Saga", "reviews": {"Jorge Smith": "Nice book."} },
+      8: {"author": "Jane Austen","title": "Pride and Prejudice", "reviews": {"Juan Gonzalez": "Pride and Prejudice is a good book!"} },
+      9: {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews": {"Cristobal Protón": "10/10"} },
+      10: {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": {"Gabe Freeman": "You should buy it!"} }
+}
+
+module.exports = books;
